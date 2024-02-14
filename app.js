687ff7d72e63105fda50883bd27beb16ef582264
ShubhamPandey8756 <shubhamias2002@gmail.com>
2024-02-14 22:14:51 +0530

    last commit

--- app.js
+++ app.js
@@ -15,8 +15,10 @@
     **/      
 // const heading=React.createElement("h1",{id:"heading"},"Hello World From React!");
 // const root=ReactDOM.createRoot(document.getElementById("root"));
-// console.log(heading);
+// console.log(heading); 
 // root.render(heading); 
+import React from "react";
+import ReactDOM from "react-dom";
 const parent=React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"i am h1 tag"),
